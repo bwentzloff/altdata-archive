939c3a57c1b78955a68ad4d99c2b888107e13672
bwentzloff <bwentzloff@users.noreply.github.com>
2026-05-17 08:46:58 -0500

    may 17 update

--- pipeline/generate_site.py
+++ pipeline/generate_site.py
@@ -139,7 +139,7 @@ def _build_sport_groups(leagues_index):
     Classify each league into a sport group and collapse AUDL/UFA as one league.
     Returns a list of groups: [{name, leagues: [{name, years: [{slug, season, player_count, game_count}]}]}]
     """
-    SPORT_ORDER = ["Football", "Soccer", "Cricket", "Curling", "Lacrosse", "Ultimate Disc", "Basketball", "Disc Golf", "Other"]
+    SPORT_ORDER = ["Football", "Basketball", "Soccer", "Cricket", "Curling", "Lacrosse", "Ultimate Disc", "Disc Golf", "Other"]
     HIDDEN = {"50 YARD", "50YARD"}   # leagues to omit from the homepage index
     FOOTBALL = {"UFL", "USFL", "XFL", "CFL", "AF1", "AAF", "ELF", "AFL", "IFL", "NAL", "LFA", "X-League", "XLEAGUE", "MLFB", "FCF"}
     BASKETBALL = {"BIG3", "SLAMBALL", "UNRIVALED", "WNBA"}
